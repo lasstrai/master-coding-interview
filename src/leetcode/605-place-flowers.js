2f3663a10769340567af1cc2349844e90a8a88f9
lasstrai <user7x9@gmail.com>
2024-10-15 18:56:54 -0500

    leetcode 844 unsolved

--- src/leetcode/605-place-flowers.js
+++ src/leetcode/605-place-flowers.js
@@ -5,23 +5,18 @@
  */
 
 const canPlaceFlowers = function(flowerbed, n) {
+  let prev, next;
   for (let i = 0; i < flowerbed.length; i++) {
     if (flowerbed[i] === 0) {
-      if (flowerbed[i - 1] === 0 && flowerbed[i + 1] === 0) {
-        flowerbed[i] = 1;
-        n--;
-      }
-      if (i === 0 && flowerbed[i + 1] === 0) {
-        flowerbed[i] = 1;
-        n--;
-      }
-      if (i === flowerbed.length - 1 && flowerbed[i - 1] === 0) {
+      prev = i === 0 || flowerbed[i - 1] === 0;
+      next = i === flowerbed.length - 1 || flowerbed[i + 1] === 0;
+      if (prev && next) {
         flowerbed[i] = 1;
         n--;
       }
     }
   }
-  return n ===  0;
+  return n === 0;
 }
 
 export default canPlaceFlowers;
---
+++ src/leetcode/844-backspace-compare.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @returns {boolean}
+ */
+
+const backspaceCompare = function(s, t) {
+  
+  function nextValidIndex(str, index) {
+    let backSpace = 0;
+    while (index >= 0) {
+      if (str.charAt(index) === "#") {
+        backSpace++;
+        index--;
+      }
+      else if (backSpace > 0 ) {
+        backSpace--;
+        index--;
+      }
+      else {
+        return index;
+      }
+    }
+    return -1;
+  }
+
+  let i = s.length - 1;
+  let j = t.length - 1;
+  while (i >= 0 || j >= 0) {
+    i = nextValidIndex(s, i);
+    j = nextValidIndex(t, j);
+    if (i>= 0 && j >= 0 & s.charAt(i) !== t.charAt(j)) {
+      return false;
+    }
+    if ((i >=0 && t < 0) || (i < 0 && t >= 0)) {
+      return false;
+    } 
+    i--;
+    j--;
+  }
+  return true;
+}
+
+export default backspaceCompare;
